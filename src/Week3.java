255fb7bacfe4fdce244cc29ff5aadf5653dea1be
mwangxx0129 <mwangxx@gmail.com>
2018-01-06 15:05:23 -0800

    add word ladder 1 method1

--- src/Week3.java
+++ src/Week3.java
@@ -138,4 +138,44 @@ public class Week3 {
         }
     }
 
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if (!wordList.contains(endWord)) return 0;
+        Set<String> dict = new HashSet<>(wordList);
+
+        Set<String> visited = new HashSet<>();
+        Deque<String> queue = new ArrayDeque<>(1000);
+
+        int count = 1;
+        queue.offerLast(beginWord);
+        visited.add(beginWord);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; ++ i) {
+                String head = queue.pollFirst();
+                if (head.equals(endWord)) return count;
+                for (String nextWord : getNextWords(head, dict))
+                    if (visited.add(nextWord)) queue.offerLast(nextWord);
+
+            }
+            ++ count;
+        }
+        return 0;
+    }
+
+    private Set<String> getNextWords(String curr, Set<String> dict) {
+        Set<String> nextWords = new HashSet<String>();
+        for (int i = 0; i < curr.length(); i++) {
+            char[] chars = curr.toCharArray();
+            for (char c = 'a'; c <= 'z'; c++) {
+                chars[i] = c;
+                String temp = new String(chars);
+                if (dict.contains(temp)) {
+                    nextWords.add(temp);
+                }
+            }
+        }
+        return nextWords;
+    }
+
 }
--- src/Week3Test.java
+++ src/Week3Test.java
@@ -1,13 +1,24 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Week3Test {
     @Test
     void permutate() {
         Week3 w3 = new Week3();
-        int[] nums = {1,2,3,4,5};
-        w3.permutate_dfs(nums);
+//        int[] nums = {1,2,3,4,5};
+//        w3.permutate_dfs(nums);
+
+        String[] dic = {"hot","dot","dog","lot","log","cog"};
+        List<String> list = new ArrayList<>();
+        for (String s : dic) {
+            list.add(s);
+        }
+        int step = w3.ladderLength("hit", "cog", list);
+        System.out.println(step);
     }
 
 }
