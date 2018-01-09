8ad49cccd534e17c297061e02cd91cfd9c608ce2
mwangxx0129 <mwangxx@gmail.com>
2018-01-08 17:06:18 -0800

    :bowtie:

--- src/Week3.java
+++ src/Week3.java
@@ -8,16 +8,20 @@ public class Week3 {
      * @return
      */
 
+    // permutate
     // bfs
     public List<List<Integer>> permutate(int[] nums) {
+//        return permutate_bfs(nums);
+        return permute_dfs_method2(nums);
+    }
+
+    public List<List<Integer>> permutate_bfs(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        if (nums == null || nums.length == 0) {
-            return res;
-        }
+        if (nums == null || nums.length == 0) return res;
         res.add(new ArrayList<Integer>());
         for (int i = 0; i < nums.length; ++ i) {
-            List<List<Integer>> nextRes = new ArrayList<>();// next level
-            for (List<Integer> list : res) {
+            List<List<Integer>> nextRes = new ArrayList<>();
+            for (List list : res) {
                 for (int j = 0; j < list.size() + 1; ++ j) {
                     List<Integer> nextList = new ArrayList<>(list);
                     nextList.add(j, nums[i]);
@@ -26,32 +30,70 @@ public class Week3 {
             }
             res = nextRes;
         }
-        System.out.println(res);
         return res;
     }
 
-    public List<List<Integer>> permutate_dfs(int[] nums) {
+    private List<List<Integer>> permute_dfs_method1(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-//        permutate_dfs_method1(nums, new ArrayList<Integer>(),  res);
-//        permutate_dfs_method2(nums, 0, new ArrayList<Integer>(), res);
-        permutate_dfs_method3(nums, 0, res);
-        System.out.println(res);
+        if (nums == null || nums.length == 0) return res;
+        permute_dfs_method1_helper(nums, new ArrayList<Integer>(), res);
         return res;
     }
-
-    // from the view of a collection, use contain to check element is in current set
-    private void permutate_dfs_method1(int[] nums, List<Integer> path, List<List<Integer>> res) {
+    private void permute_dfs_method1_helper(int[] nums, List<Integer> path, List<List<Integer>> res) {
         if (path.size() == nums.length) {
-            res.add(new ArrayList<>(path));
+            res.add(new ArrayList<Integer>(path));
             return;
         }
-        for(int i = 0; i < nums.length; ++ i) {
-            if(path.contains(nums[i])) continue;
+        for (int i = 0; i < nums.length; ++ i) {
+            if (!path.contains(nums[i])) {
+                path.add(nums[i]);
+                permute_dfs_method1_helper(nums, path, res);
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+    private List<List<Integer>> permute_dfs_method2(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) return res;
+        permute_dfs_method2_helper(nums, 0, new ArrayList<Integer>(), res);
+        return res;
+    }
+    private void permute_dfs_method2_helper(int[] nums, int startIndex, List<Integer> path, List<List<Integer>> res) {
+        if (path.size() == nums.length) {
+            res.add(new ArrayList<Integer>(path));
+            return;
+        }
+        for (int i = startIndex; i < nums.length; ++ i) {
             path.add(nums[i]);
-            permutate_dfs_method1(nums, path, res);
+            swap(nums, startIndex, i);
+            permute_dfs_method2_helper(nums, startIndex + 1, path, res);
+            swap(nums, startIndex, i);
             path.remove(path.size() - 1);
         }
     }
+
+    private List<List<Integer>> permute_dfs_method3(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) return res;
+        permute_dfs_method3_helper(nums, 0, res);
+        return res;
+    }
+    private void permute_dfs_method3_helper(int[] nums, int level, List<List<Integer>> res) {
+        if (level == nums.length - 1) {
+            List<Integer> list = new ArrayList<>();
+            for (int e : nums)
+                list.add(e);
+            res.add(list);
+            return;
+        }
+        for (int i = level; i < nums.length; ++ i) {
+            swap(nums, level, i);
+            permute_dfs_method3_helper(nums, level + 1, res);
+            swap(nums, level, i);
+        }
+    }
+
+
 
     // swap array, save path, push to res
     public void permutate_dfs_method2(int[] nums, int level, List<Integer> path, List<List<Integer>> res) {
@@ -72,23 +114,6 @@ public class Week3 {
         nums[l] ^= nums[r];
         nums[r] ^= nums[l];
         nums[l] ^= nums[r];
-    }
-
-    // swap array, just push leaf node set to res
-    public void permutate_dfs_method3(int[] nums, int level, List<List<Integer>> res) {
-        if (level == nums.length - 1) {
-            List<Integer> path = new ArrayList<>();
-            for (int num : nums) {
-                path.add(num);
-            }
-            res.add(path);
-            return;
-        }
-        for (int i = level; i < nums.length; ++ i) {
-            swap(nums, level, i);
-            permutate_dfs_method3(nums, level + 1, res);
-            swap(nums, level, i);
-        }
     }
 
     public List<List<Integer>> permuteUnique(int[] nums) {
