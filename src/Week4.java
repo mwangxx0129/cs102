725d7970395ea78f8a6b1a3d899c118391118804
mwangxx0129 <mwangxx@gmail.com>
2018-02-08 12:15:34 -0800

    jump game

--- src/Week4.java
+++ src/Week4.java
@@ -235,4 +235,35 @@ public class Week4 {
             return min;
         }
     }
+
+    class SolutionJumpGame {
+        public boolean canJump(int[] nums) {
+            if (nums == null || nums.length == 0) return false;
+            int lastIndex = nums.length - 1;
+            for (int i = nums.length - 1; i >= 0; -- i)
+                if (nums[i] + i >= lastIndex)
+                    lastIndex = i;
+            return lastIndex == 0;
+        }
+    }
+
+    class SolutionJumpGameII {
+        public int jump(int[] nums) {
+            if (nums == null || nums.length == 0) return -1;
+            int n = nums.length;
+            int[] min = new int[n];
+            min[0] = 0;
+            int curIndex = 0;
+            for (int i = 1; i < nums.length; ++ i) {
+                for (int j = curIndex; j < i; ++ j) {
+                    if (nums[j] + j >= i) {
+                        min[i] = min[j] + 1;
+                        curIndex = j;
+                        break;
+                    }
+                }
+            }
+            return min[nums.length - 1];
+        }
+    }
 }
