edc9a3b0189d557d231ca0ab5a8179cda68c4abf
mwangxx0129 <mwangxx@gmail.com>
2018-02-09 22:53:17 -0800

    Interleaving String

--- src/Week4.java
+++ src/Week4.java
@@ -288,4 +288,30 @@ public class Week4 {
         }
     }
 
+    class SolutionIsInterleave {
+        public boolean isInterleave(String s1, String s2, String s3) {
+            int m = s1.length();
+            int n = s2.length();
+            int k = s3.length();
+            if (m + n != k) return false;
+            if (s1.isEmpty()) return s2.equals(s3);
+            if (s2.isEmpty()) return s1.equals(s3);
+
+            boolean[][] dp = new boolean[m + 1][n + 1];
+            dp[0][0] = true;
+            for (int i = 1; i < m + 1; ++ i) {
+                dp[i][0] = dp[i - 1][0] && s1.charAt(i - 1) == s3.charAt(i - 1);
+            }
+            for (int i = 1; i < n + 1; ++ i) {
+                dp[0][i] = dp[0][i - 1] && s2.charAt(i - 1) == s3.charAt(i - 1);
+            }
+            for (int i = 1; i < m + 1; ++ i) {
+                for (int j = 1; j < n + 1; ++ j) {
+                    char c = s3.charAt(i + j - 1);
+                    dp[i][j] = (c == s1.charAt(i - 1) && dp[i - 1][j]) || (dp[i][j - 1] && c == s2.charAt(j - 1));
+                }
+            }
+            return dp[m][n];
+        }
+    }
 }
